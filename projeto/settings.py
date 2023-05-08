373b23f00708fc5907bc9c71c485e2cd7fb9f3ca
Samuel Santos Gomes de Souza <Samuels.g.desouza@gmail.com>
2023-05-08 17:56:02 -0300

    alterando o debug para false

--- projeto/settings.py
+++ projeto/settings.py
@@ -32,12 +32,12 @@ SECURE_SSL_REDIRECT = False
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 # Application definition
 
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "False"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
