ba3ee0469473ac6a47986eacde8a3f8b9a123761
Leticia Bora <l.bora@electroneek.com>
2024-09-21 12:18:59 -0300

    change folders

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,34 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="w-full h-full">
-      <main>
-        <div>nutri</div>
+      <main className="flex flex-col w-full h-full">
+        {/* <div className="flex flex-col lg:flex-row h-full w-full gap-20">
+          <div class="relative bg-defaultBlue rounded-full flex items-center justify-center text-white text-xl w-[100vw] h-[100vw] max-w-[600px] max-h-[600px] overflow-hidden">
+            <div className="absolute inset-0 -bottom-5 flex items-end justify-center">
+              <Image
+                src="/images/foto_israel.png"
+                alt="Imagem"
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+          </div>
+          <div class="flex flex-col gap-5 w-full h-full">
+            <h1 className="font-semibold text-4xl">
+            Transforme sua vida com emagrecimento definitivo!
+            </h1>
+            <p>
+            Sou um Nutricionista Esportivo com 15 anos de experiência ajudando milhares a alcançar seus objetivos de forma eficaz e sustentável. Comigo, você pode continuar desfrutando dos alimentos que gosta, como pão, arroz, pizza e cerveja, enquanto trabalha para emagrecer de maneira prática e adaptada ao seu estilo de vida. </p>
+
+            <p className="font-bold">Faça login agora e assista ao vídeo explicativo para descobrir como posso ajudar você a atingir suas metas e transformar sua vida.</p>
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/2U0k54fw3Ag?si=f1n4EXZksIt3klnQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            <p>Vamos juntos alcançar o emagrecimento definitivo e a vida que você merece!</p>
+          </div>
+        </div> */}
       </main>
     </div>
   );
