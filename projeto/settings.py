af42bf0d424cc8019f37e44e8cb90756c5d21e1d
DiegoBuencuero <diegobuencuero4095@gmail.com>
2025-10-09 22:05:11 -0300

    allow

--- projeto/settings.py
+++ projeto/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 #DEBUG = False
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'israeladolfo.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'israeladolfo.pythonanywhere.com', 'emagrecimentodefinitivo.app.br']
 
 
 
